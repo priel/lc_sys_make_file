1a8130f81b500397b72777b301905677b07bbf62
spio <spio@phony5.technion.ac.il>
2017-08-08 20:05:32 +0300

    oshri's first commit

--- include/defined.h
+++ include/defined.h
@@ -5,40 +5,40 @@
 #define K_B 1.38066e-23
 
 /// model defined:
-#define MODEL_NAME "3D_5x5x5_no_col_only_spins" ///the model name, this will be the folder name in the run directory
+#define MODEL_NAME "3D_11x11x11" ///the model name, this will be the folder name in the run directory
 
 #define DIMENSIONS 3
-#define MOLECULES_IN_EACH_DIRECTION {9,9,9} /// #moleuclues will be the product of all elements.
-#define SYSTEM_SIZES {1000,1000,1000} /// the location of the molecules cant exceed the size of the system.
+#define MOLECULES_IN_EACH_DIRECTION {15,15,15} /// #moleuclues will be the product of all elements.
+#define SYSTEM_SIZES {15,15,15} /// the location of the molecules cant exceed the size of the system.
 
 /// colloid molecules - define all the molecules in the system
 #define COLLOID_MOLS {\
-    {4,4,4}\
+    {7,7,7}\
 }
 
 //#define IGNORE_COLLOIDS ///this defined tell the program to ignore colloid molecules, comment in order to use colloids.
 
-#define INIT_SPACING 1.0 ///the expected value of the inital location of the molecules
+#define INIT_SPACING 0.9 ///the expected value of the inital location of the molecules
 #define INIT_SPIN {1/sqrt(3), 1/sqrt(3), 1/sqrt(3)} ///the expected value of the inital orientation of the molecules
 
 #define INIT_SPACING_STD 0.1 ///the standart deviation of the inital location of the location
 #define INIT_SPIN_STD 1.0 ///the standart deviation of the inital location of the spin
 //can't be zero!
 
-#define TEMPERATURE_RANGE {3.0, 2.8, 2.5, 2.0, 1.75, 1.5, 1.25, 1.0, 0.9, 0.8, 0.7, 0.6, 0.5, 0.4, 0.3, 0.2, 0.1, 0.05, 0.01, 0.001}  ///temperature range of the monte carlo
+#define TEMPERATURE_RANGE {3.0, 2.9,2.8,2.7,2.6, 2.5,2.4,2.3,2.2,2.1, 2.0, 1.9,1.8,1.7,1.6, 1.5,1.4,1.3,1.2,1.1, 1.0, 0.9, 0.8, 0.7, 0.6, 0.5, 0.4, 0.3, 0.2, 0.1, 0.05, 0.01,0.005, 0.001}  ///temperature range of the monte carlo
 
-#define NUMBER_OF_STEPS 1000000 ///number of steps in each temperature
+#define NUMBER_OF_STEPS 500000 ///number of steps in each temperature
 
-#define STD_LOCATION 0.01 ///the standart deviation of the location in the monte carlo
-#define STD_SPIN 0.2 ///the standart deviation of the orientation in the monte carlo
+#define STD_LOCATION 0.1 ///the standart deviation of the location in the monte carlo
+#define STD_SPIN 0.15 ///the standart deviation of the orientation in the monte carlo
 //can't be zero!
 
 //L > D
-#define D_0 0.3 /// the small size of the liquid crystal molecule.
-#define L_0 1.0 /// the long size of the liquid crystal molecule
+#define D_0 0.2 /// the small size of the liquid crystal molecule.
+#define L_0 1 /// the long size of the liquid crystal molecule
 
-#define D_1 4.5 /// the radius of the colloid molecule
-#define L_1 4.5001 ///D_1 + very small number, just to allow the expression (L_1 - D_1) / (L_1 - D_1)
+#define D_1 7.5 /// the radius of the colloid molecule
+#define L_1 7.5001 ///D_1 + very small number, just to allow the expression (L_1 - D_1) / (L_1 - D_1)
 
 #define EPSILON0_0 K_B ///the epsilon0 of the LC
 #define EPSILON0_1 K_B ///the epsilon0 of the col
